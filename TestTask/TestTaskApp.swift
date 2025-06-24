89fabb0a813a82a79e7a7436bc5eb3314e6f7ead
ChernovKonstantin <chernov.konstantin@gmail.com>
2025-06-24 14:09:40 +0200

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Chernov Kostiantyn on 24.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
